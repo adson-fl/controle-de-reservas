12aa73302eb89d9b6e2bcc06d15079e8f457c677
Adson Leite <adsonferreira585@gmail.com>
2022-10-15 23:28:07 -0300

    contorle de quartos reservados

---
+++ Calc/Benk.java
@@ -0,0 +1,33 @@
+package Calc;
+
+public class Benk {
+	private String nome;
+	private String email;
+
+	public Benk(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String toString() {
+		return nome + "," + email;
+	}
+
+}
---
+++ Enteracao/Executar.java
@@ -0,0 +1,42 @@
+package Enteracao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import Calc.Benk;
+
+public class Executar {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Benk[] vect = new Benk[10];
+		System.out.println("digite quantos quartos serão alugados:");
+		int quarto = sc.nextInt();
+
+		for (int i = 1; i <= quarto; i++) {
+			sc.nextLine();
+			System.out.printf("%s° alugel\n", i + ":");
+			System.out.println("digite o nome:");
+			String nome = sc.nextLine();
+			System.out.println("digite o E-MAIL:");
+			String email = sc.nextLine();
+			System.out.println("digite o numero do quarto");
+			int numerodoquarto = sc.nextInt();
+
+			vect[numerodoquarto] = new Benk(nome, email);// atribui o obijeto na posição do verrtor estanciado
+		}
+		System.out.println("");
+		System.out.println("chequim");
+		for (int i = 0; i < 10; i++) {
+			if (vect[i] != null) {
+				System.out.println(i + "; " + vect[i]);
+			}
+
+		}
+		sc.close();
+
+	}
+
+}
